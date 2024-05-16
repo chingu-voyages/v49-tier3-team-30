3cb5a016f9eb462f295cfacbc7cd7e82e99fd18c
NadiaPia <206i417@gmail.com>
2024-05-16 15:01:20 -0700

    added Router to App.js

--- study-app/src/App.jsx
+++ study-app/src/App.jsx
@@ -2,16 +2,30 @@
 
 import './App.css'
 import './components/FrontEndRoadmap/FrontEndRoadmap.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import FrontEndRoadmap from './components/FrontEndRoadmap/FrontEndRoadmap';
+import Navbar from './components/Navbar/Navbar';
+
 
 
 function App() {
 
   return (
-    <div>
-      <FrontEndRoadmap/>
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<FrontEndRoadmap />}/>
+          {/* 
+          <Route path="/login" element={<Login />}/>
+          <Route path="/forgot-password" element={<ForgotPassword />}/>
+          */}
+
+        </Routes>
+      </Router>
+      
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ study-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      Our Navbar can be here
+    </div>
+  )
+}
+
+export default Navbar
