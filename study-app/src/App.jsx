ac170c719e1953c39871a35db00b24007492dce0
NadiaPia <206i417@gmail.com>
2024-05-18 02:22:42 -0700

    cleaned up code in App.jsx

--- study-app/src/App.jsx
+++ study-app/src/App.jsx
@@ -6,19 +6,10 @@ import './components/LeftSideMenu/LeftSideMenu.css'
 import './components/Main/Main.css'
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import FrontEndRoadmap from './components/FrontEndRoadmap/FrontEndRoadmap';
 import Navbar from './components/Navbar/Navbar';
-import LeftSideMenu from './components/LeftSideMenu/LeftSideMenu';
-import { useState } from 'react';
 import Main from './components/Main/Main';
 
-
-
-
-
-function App() {
-
-  
+function App() { 
 
   return (
     <div>
