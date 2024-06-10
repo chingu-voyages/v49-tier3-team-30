63454ed7d9b1e6a58c528e73ba4b76caa7801a25
NadiaPia <87682711+NadiaPia@users.noreply.github.com>
2024-06-10 10:01:19 -0700

    Nadia login page (#39)
    * added login FE
    
    * added login and logout functionality
    
    * added dropdown menu in navbar
    
    * fixed style
    
    * replaced a element  with the p element
    
    * replaced p elements with the div
    
    * fixed style
    
    * fixed greetings in dropdown menu
    
    * added hide dropdown menu on click outside
    
    * fixed bugs

--- study-app/src/components/Login/index.jsx
+++ study-app/src/components/Login/index.jsx
@@ -1,7 +1,87 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
 
-function Login() {
-  return <div>Login</div>;
+function Login({ setAuthState }) {
+  const serverUrl = import.meta.env.VITE_SERVER_URL;
+  const navigate = useNavigate();
+  const [inputData, setInputData] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState({ username: false, password: false });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setInputData((prev) => ({ ...prev, [name]: value }));
+    if (value.trim() !== "") {
+      setErrors((prev) => ({ ...prev, [name]: false }));
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newErrors = {
+      username: inputData.username.trim() === "",
+      password: inputData.password.trim() === "",
+    };
+    setErrors(newErrors);
+
+    if (newErrors.username || newErrors.password) {
+      console.log("Some inputs are empty");
+      return;
+    }
+
+    try {
+      const response = await axios.post(`${serverUrl}/user/login`, inputData);
+      setAuthState({
+        username: response.data.username,
+        id: response.data.userId,
+        status: true,
+      });
+      setInputData({ username: "", password: "" });
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="signUpContainer">
+      <div className="card">
+        <form className="inputsContainer" onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            name="username"
+            type="text"
+            placeholder="Username..."
+            value={inputData.username}
+            className={errors.username ? "inputEmpty" : "input"}
+          />
+
+          <input
+            onChange={handleChange}
+            name="password"
+            type="text"
+            placeholder="Password..."
+            value={inputData.password}
+            className={errors.password ? "inputEmpty" : "input"}
+          />
+
+          <Link to="forgot-password" className="forgotPasswordLink">
+            Forgot Password
+          </Link>
+
+          <button className="submitBtn" type="submit">
+            Login
+          </button>
+        </form>
+        <div className="signUpLinkContainer">
+          <div>Don't have an account?</div>
+          <div className="signUpLink" onClick={() => navigate("/signup")}>
+            Sign Up
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- study-app/src/components/Navbar/Navbar.jsx
+++ study-app/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,95 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import "./Navbar";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import user from "../../img/user.png";
+import logout from "../../img/log-out.png";
+import { useState, useEffect, useRef } from "react";
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 
-function Navbar() {
-	return (
-		<div className="navbarSection">
-			<Link to="" className="navLink">
-				Home
-			</Link>
-			<Link to="profile" className="navLink">
-				Profile
-			</Link>
-			<Link to="login" className="navLink" id="loginLink">
-				Login
-			</Link>
-		</div>
-	);
+function Navbar({ authState, setAuthState }) {
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const username = authState.username;
+  let menuRef = useRef();
+
+  const handleLogout = async () => {
+    const response = await axios.get(`${serverUrl}/user/logout`);
+    setAuthState({ username: "", id: 0, status: false });
+    console.log("response", response.data);
+  };
+
+  //functionality to close dropdown menu if click outside of it-------------
+  useEffect(() => {
+    let handler = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  }, [menuRef]);
+  //---------------------------------------------------------------------------
+
+  return (
+    <div className="navbarSection">
+      <Link to="" className="navLink">
+        Home
+      </Link>
+
+      {authState.status ? (
+        <div className="menu-container" ref={menuRef}>
+          <div
+            className="menu-trigger"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            <div className="account-trigger navLink">Account ▾</div>
+            <div className={`dropdown-menu ${open ? "active" : "inactive"}`}>
+              <h3>{username ? `Hello, ${username}` : ""}! </h3>
+              <ul>
+                <DropdownItem
+                  img={user}
+                  text={
+                    <div
+                      className="dropdown-menu-links"
+                      onClick={() => navigate("/profile")}
+                    >
+                      {" "}
+                      My Profile{" "}
+                    </div>
+                  }
+                />
+                <DropdownItem
+                  img={logout}
+                  text={
+                    <div className="dropdown-menu-links" onClick={handleLogout}>
+                      Logout
+                    </div>
+                  }
+                />
+              </ul>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Link to="login" className="navLink" id="loginLink">
+          Login
+        </Link>
+      )}
+    </div>
+  );
+}
+
+function DropdownItem(props) {
+  return (
+    <li className="dropdownItem">
+      <img src={props.img}></img>
+      <div>{props.text}</div>
+    </li>
+  );
 }
 
 export default Navbar;
